186d38b9557f609323b6c63144796e779fece9a6
Александр <Sanfor228@mail.ru>
2022-09-14 20:04:26 +0400

    c2

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 AUTH_USER_MODEL = 'users.CustomUser'
 #
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
 #
 #
 #
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
+
 
 # Create your views here.
+class HomePageView(ListView):
+    model = models.Shop
+    template_name = 'shop/home.html'
